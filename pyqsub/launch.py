7a3f040cc2ba7af06c8834dfb07d1c3bade1e4d1
Robin Strudel <rstrudel@gmail.com>
2019-10-18 03:24:14 +0200

    Refactor launch

--- pyqsub/launch.py
+++ pyqsub/launch.py
@@ -9,54 +9,66 @@ import yaml
 from pyqsub.settings import LOG_DIR, PBS_DIR, CFG_DIR
 
 
-def create_pbs_template(pbs_dir, pbs_file):
-    pbs_template = ''
+def create_template(pbs_dir, pbs_file):
+    template = ''
     with open(os.path.join(pbs_dir, 'header.pbs'), 'r') as f:
-        pbs_template = f.read()
+        template = '# HEADER\n'+f.read()
     with open(os.path.join(pbs_dir, pbs_file), 'r') as f:
-        pbs_template += f.read()
-    return pbs_template
+        template += '\n\n# EXPERIMENT\n'+f.read()
+    return template
 
 
-def launch_jobs(pbs_template, pbs_args, user_dict_args, no_qsub):
+def launch_jobs(template, args, config, no_qsub):
     # write pbs and launch jobs
-    list_dict_args = list(ParameterGrid(user_dict_args))
-    for arg in pbs_args:
+    list_dict_args = list(ParameterGrid(config))
+    for arg in args:
         assert arg in list_dict_args[
-            0], '{} is needed by pbs but is not defined.'.format(arg)
+            0], '{} is needed by the template but is not defined.'.format(arg)
 
     pbs_file = 'temp.pbs'
     for dict_args in list_dict_args:
         print(dict_args)
         with open(pbs_file, 'w') as f:
-            pbs_content = pbs_template.format(**dict_args)
+            pbs_content = template.format(**dict_args)
             f.write(pbs_content)
         if not no_qsub:
             subprocess.run(['qsub', pbs_file])
-    os.remove(pbs_file)
+    # os.remove(pbs_file)
+    if not no_qsub:
+        print('{} jobs launched.'.format(len(list_dict_args)))
 
+def parse_template(template):
+    args = list(
+        set([
+            name for _, name, _, _ in Formatter().parse(template)
+            if name is not None
+        ]))
+    return args
+
+def show_experiment(template, args, config_file):
+    print('Arguments:\n{}\n'.format(args))
+    with open(config_file) as f:
+        print('Config:\n{}\n'.format(f.read()))
+    print('Template (.pbs):\n{}'.format(template))
 
 @click.command()
 @click.argument('pbs-file', type=str)
 @click.argument('yaml-file', type=str)
-@click.option('--show-args/--no-show-args', default=False)
+@click.option('--show/--no-show', default=False)
 @click.option('--no-qsub/--no-no-qsub', default=False)
-def main(pbs_file, yaml_file, show_args, no_qsub):
+def main(pbs_file, yaml_file, show, no_qsub):
     # create pbs string
-    pbs_template = create_pbs_template(PBS_DIR, pbs_file)
-    pbs_args = list(
-        set([
-            name for _, name, _, _ in Formatter().parse(pbs_template)
-            if name is not None
-        ]))
-    if show_args:
-        print('Pbs Arguments: {}'.format(pbs_args))
-        print('Pbs Template:\n {}'.format(pbs_template))
+    template = create_template(PBS_DIR, pbs_file)
+    args = parse_template(template)
+    config_file = os.path.join(CFG_DIR, yaml_file)
+    if show:
+        show_experiment(template, args, config_file)
+        print('Experiment not launched!')
         return
 
-    with open(os.path.join(CFG_DIR, yaml_file)) as f:
-        user_dict_args = yaml.load(f, Loader=yaml.FullLoader)
-    launch_jobs(pbs_template, pbs_args, user_dict_args, no_qsub)
+    with open(config_file) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    launch_jobs(template, args, config, no_qsub)
 
 
 if __name__ == '__main__':
