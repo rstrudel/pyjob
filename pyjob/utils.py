7cd2396da1dd1f7320b62a25719b97e6fc1bda10
rstrudel <rstrudel@gmail.com>
2020-04-10 16:36:14 +0200

    scheduler: add oar

--- pyjob/utils.py
+++ pyjob/utils.py
@@ -1,91 +1,104 @@
 import os
-from sklearn.model_selection import ParameterGrid
-import yaml
 from string import Formatter
-from termcolor import colored
 
+import yaml
 from pyjob.settings import BASE_DIR
+from sklearn.model_selection import ParameterGrid
+from termcolor import colored
 
 
 def get_scheduler_infos(scheduler):
     scheduler_dict = {
-        'sge': {
-            'header': 'header.pbs',
-            'extension': 'pbs',
-            'submit_command': 'qsub',
-            'config': 'default_sge.yml',
+        "sge": {
+            "header": "header.pbs",
+            "extension": "pbs",
+            "submit_command": "qsub",
+            "config": "default_sge.yml",
         },
-        'slurm': {
-            'header': 'header.slurm',
-            'extension': 'slurm',
-            'submit_command': 'sbatch',
-            'config': 'default_slurm.yml',
-        }
+        "slurm": {
+            "header": "header.slurm",
+            "extension": "slurm",
+            "submit_command": "sbatch",
+            "config": "default_slurm.yml",
+        },
+        "oar": {
+            "header": "header.oar",
+            "extension": "oar",
+            "submit_command": "oarsub --scanscript",
+            "config": "default_oar.yml",
+        },
     }
     if scheduler not in scheduler_dict:
-        raise ValueError('{} is an unknown scheduler. Supported schedulers: {}'.format(
-            scheduler, scheduler_dict.keys()))
+        raise ValueError(
+            "{} is an unknown scheduler. Supported schedulers: {}".format(
+                scheduler, scheduler_dict.keys()
+            )
+        )
     return scheduler_dict[scheduler]
 
 
 def make_dirs(jobs_log_dir):
-    scripts_dir = [os.path.join(jobs_log_dir, 'scripts')]
+    scripts_dir = [os.path.join(jobs_log_dir, "scripts")]
     for exp_dir in scripts_dir:
         if not os.path.exists(exp_dir):
             os.makedirs(exp_dir, exist_ok=True)
 
 
 def create_template(scheduler_infos, template):
-    header_file = os.path.join(BASE_DIR, 'header', scheduler_infos['header'])
-    template_file = os.path.join(BASE_DIR, 'template', template)
+    header_file = os.path.join(BASE_DIR, "header", scheduler_infos["header"])
+    template_file = os.path.join(BASE_DIR, "template", template)
 
-    template = ''
-    with open(header_file, 'r') as f:
+    template = ""
+    with open(header_file, "r") as f:
         template = f.read()
-    with open(template_file, 'r') as f:
-        template += '\n\n# EXPERIMENT\n' + f.read()
+    with open(template_file, "r") as f:
+        template += "\n\n# EXPERIMENT\n" + f.read()
     return template
 
 
 def parse_template(template):
-    args = list(set([name for _, name, _, _ in Formatter().parse(template) if name is not None]))
+    args = list(
+        set([name for _, name, _, _ in Formatter().parse(template) if name is not None])
+    )
     return args
 
 
 def load_config(schduler_infos, config_file):
-    default_config_file = os.path.join(BASE_DIR, 'config', schduler_infos['config'])
-    user_config_file = os.path.join(BASE_DIR, 'config', config_file)
+    default_config_file = os.path.join(BASE_DIR, "config", schduler_infos["config"])
+    user_config_file = os.path.join(BASE_DIR, "config", config_file)
     with open(default_config_file) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     with open(user_config_file) as f:
         user_config = yaml.load(f, Loader=yaml.FullLoader)
     config.update(user_config)
 
-    required_args = ['job_log_dir', 'job_name']
+    required_args = ["job_log_dir", "job_name"]
     for arg in required_args:
         if arg not in config:
-            raise ValueError('{} should be defined in config.'.format(arg))
-    if len(config['job_name']) > 1:
-        raise ValueError('Only one job_name is allowed per experiment.')
+            raise ValueError("{} should be defined in config.".format(arg))
+    if len(config["job_name"]) > 1:
+        raise ValueError("Only one job_name is allowed per experiment.")
     return config
 
 
 def create_args_from_config(template_args, config):
     unused_config_args = [arg for arg in config.keys() if arg not in template_args]
-    print('Unused configuration parameters: {}'.format(unused_config_args))
+    print("Unused configuration parameters: {}".format(unused_config_args))
     config = {k: v for k, v in config.items() if k not in unused_config_args}
     list_dict_args = list(ParameterGrid(config))
     for arg in template_args:
         if arg not in list_dict_args[0]:
-            raise ValueError('{} is a template argument but is not defined in config.'.format(arg))
+            raise ValueError(
+                "{} is a template argument but is not defined in config.".format(arg)
+            )
     return list_dict_args
 
 
 def show_submission(template, args, config):
-    color = 'yellow'
-    print(colored('Template:', color))
-    print('{}\n'.format(template))
-    print(colored('Arguments:', color))
-    print('{}\n'.format(args))
-    print(colored('Config:', color))
-    print('{}'.format(yaml.dump(config)))
+    color = "yellow"
+    print(colored("Template:", color))
+    print("{}\n".format(template))
+    print(colored("Arguments:", color))
+    print("{}\n".format(args))
+    print(colored("Config:", color))
+    print("{}".format(yaml.dump(config)))
